b19b8e2f20d81654e138f165a4ca3403bbf20e83
Carlos Augusto Silva <carlos.silva1@ibm.com>
2024-12-04 18:49:27 -0300

    Formats main

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ terraform {
 
 provider "azurerm" {
 
-  subscription_id = "19c90abf-d616-4f8c-b887-f0490119b05a"
-  client_id       = "f9ae38b8-7b03-4817-9039-39cb4774621d"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
   use_oidc        = true
-  tenant_id       = "0dcd7d6a-ba5c-44b2-8858-b89a508cc2fd"
 
   features {}
 
