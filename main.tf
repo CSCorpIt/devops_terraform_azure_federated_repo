2b92a374935b49f0c18cc4d462b8aaf0855a93c0
Carlos Augusto Silva <carlos.silva1@ibm.com>
2024-12-13 16:37:43 -0300

    Removes double quotes from resources names

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.solutionName}}-vnet"
+  name                = "${var.solutionName}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
