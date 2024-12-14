c5f6423b36572e6502910bd4c63d2c3292990134
Carlos Augusto Silva <carlos.silva1@ibm.com>
2024-12-14 00:07:48 -0300

    Removes double quotes from resources names

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
